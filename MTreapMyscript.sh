eb33398b233a97ad58447230de540d55c2947016
kapilkumar30 <80621227+kapilkumar30@users.noreply.github.com>
2025-08-03 21:08:29 +0530

    Update MTreapMyscript.sh

--- MTreapMyscript.sh
+++ MTreapMyscript.sh
@@ -1,5 +1,5 @@
 echo "90-9-1"
-echo "90-9-1" >> MTreap_output3.txt
+echo "90-9-1" >> MTreap_output.txt
 fat_size=8
 while [ $fat_size -le 128 ]
 do
@@ -14,10 +14,10 @@ do
             	do
 					if [ $n_th -le 8 ]
                   	then
-			java M_Treap_Mytest2 $n_th $range 60000 $fat_size 90 99 >> MTreap_output3.txt
+			java M_Treap_Mytest2 $n_th $range 60000 $fat_size 90 99 >> MTreap_output.txt
                     elif [ $n_th -le 64 ]
                     then 
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 90 99 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 90 99 >> MTreap_output.txt
                      fi
                 ((iter=iter+1))
                 done
@@ -28,7 +28,7 @@ do
 ((fat_size=fat_size*4))
 done
 echo "70-20-10"
-echo "70-20-10" >> MTreap_output3.txt
+echo "70-20-10" >> MTreap_output.txt
 fat_size=8
 while [ $fat_size -le 128 ]
 do
@@ -43,10 +43,10 @@ do
                 do
                     if [ $n_th -le 8 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 70 90 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 70 90 >> MTreap_output.txt
                     elif [ $n_th -le 64 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 70 90 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 70 90 >> MTreap_output.txt
                      fi
                 ((iter=iter+1))
                 done
@@ -57,7 +57,7 @@ do
 ((fat_size=fat_size*4))
 done
 echo "50-25-25"
-echo "50-25-25" >> MTreap_output3.txt
+echo "50-25-25" >> MTreap_output.txt
 fat_size=8
 while [ $fat_size -le 128 ]
 do
@@ -72,10 +72,10 @@ do
                 do
                     if [ $n_th -le 8 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 50 75 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 50 75 >> MTreap_output.txt
                     elif [ $n_th -le 64 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 50 75 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 50 75 >> MTreap_output.txt
                      fi
                 ((iter=iter+1))
                 done
@@ -86,7 +86,7 @@ do
 ((fat_size=fat_size*4))
 done
 echo "30-35-35"
-echo "30-35-35" >> MTreap_output3.txt
+echo "30-35-35" >> MTreap_output.txt
 fat_size=8
 while [ $fat_size -le 128 ]
 do
@@ -101,10 +101,10 @@ do
                 do
                     if [ $n_th -le 8 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 30 65 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 30 65 >> MTreap_output.txt
                     elif [ $n_th -le 64 ]
                     then
-                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 30 65 >> MTreap_output3.txt
+                    java M_Treap_Mytest2 $n_th $range 60000 $fat_size 30 65 >> MTreap_output.txt
                      fi
                 ((iter=iter+1))
                 done
